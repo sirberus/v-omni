c2aaf21c99006982c0889917b4ef5d939b02b935
Patrick Keenan <sirberus.io@gmail.com>
2019-02-13 15:25:49 -0800

    init _mdi and wire icon

--- src/components/Icon/index.vue
+++ src/components/Icon/index.vue
@@ -1,13 +1,16 @@
-<template>
-  <div><slot>Yo</slot></div>
+<template functional>
+  <div class="icon-wrapper">
+    <span :class="`icon mdi mdi-${props.icon}`"></span>
+  </div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    icon: {
+      type: String,
+      required: true
+    }
+  }
 }
 </script>
-
-<style>
-
-</style>
