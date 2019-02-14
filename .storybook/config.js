c8b3a1371568b34475174d9f5b00bc2fac9b1c2c
Patrick Keenan <sirberus.io@gmail.com>
2019-02-13 16:35:49 -0800

    storybook basic configuration

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,22 @@
-import { configure } from '@storybook/vue';
+import { configure, addDecorator } from '@storybook/vue';
+
+import Vue from 'vue';
+import Omni from '../src/index.js';
+
+Vue.use(Omni)
 
 const req = require.context('../src/components', true, /\.stories\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addDecorator(() => ({
+  template: `
+    <div class="text-center">
+      <story/>
+    </div>
+  `
+}))
 
 configure(loadStories, module);
