7393e069c830cf24a1f29d888dcdc347ffd94d1c
Patrick Keenan <sirberus.io@gmail.com>
2019-02-16 20:43:12 -0800

    storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,22 +1,35 @@
 import { configure, addDecorator } from '@storybook/vue';
 
+
+/* Install vue plugin to globally register components */
 import Vue from 'vue';
 import Omni from '../src/index.js';
-
 Vue.use(Omni)
 
-const req = require.context('../src/components', true, /\.stories\.js$/);
 
+/* 
+  Configure addon: viewports
+  https://github.com/storybooks/storybook/tree/master/addons/viewport 
+*/
+import { configureViewport } from '@storybook/addon-viewport';
+configureViewport()
+
+
+/* 
+  Configure addon: console 
+  https://github.com/storybooks/storybook-addon-console
+*/
+// import '@storybook/addon-console';
+// import { withConsole } from '@storybook/addon-console';
+// addDecorator((storyFn, context) => withConsole()(storyFn)(context)); 
+
+
+/* Dynamically import and load stories */
+const req = require.context('../src/components', true, /\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
 
-addDecorator(() => ({
-  template: `
-    <div class="text-center">
-      <story/>
-    </div>
-  `
-}))
 
+/* Registering the configuration */
 configure(loadStories, module);
